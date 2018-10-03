850503ea2a4d9b1f03d25fc4aa05bbf62b251ab7
Rafael Arce Nazario <rafael.arce@upr.edu>
2018-10-03 06:55:42 -0400

    initial

--- doubleStackAll.cpp
+++ doubleStackAll.cpp
@@ -4,24 +4,7 @@
 using namespace std;
 
 
-/*-- DStack declaration----------------------------------------------------
- 
-  This header file defines a Stack data type.
-  Basic operations:
-    constructor:  Constructs an empty stack
-    empty:        Checks if the specified stack is empty
-    push:         Modifies a stack by adding a value at the top
-    top:          Retrieves the top stack value; leaves stack unchanged
-    pop:          Modifies stack by removing the value at the top
-    display:      Displays all the stack elements
-    full:         Checks if the stack is full.
 
-  Class Invariant:
-    1. Stack #0 elements are stored in positions
-       0, 1, . . ., myTop0 of myArray.
-    2. Stack #1 elements are stored in positions
-       STACK_CAPACITY, STACK_CAPACITY-1, ....myTop1, 
---------------------------------------------------------------------------*/
 
 const int STACK_CAPACITY = 6;
 typedef int StackElement;
@@ -29,32 +12,13 @@ typedef int StackElement;
 class DStack {
  public:
   DStack();
-  /*------------------------------------------------------------------------
-    Construct a DStack object.
-   -----------------------------------------------------------------------*/
 
-  bool empty(int stackNum) const;
-  /*------------------------------------------------------------------------
-    Check if stack is empty.
-   -----------------------------------------------------------------------*/
-
-  bool full() const;
+  bool isEmpty(int stackNum) const;
+  bool isFull() const;
 
   void push(int stackNum, const StackElement & value);
-  /*------------------------------------------------------------------------
-    Add a value to one of the stacks.
-    The stackNum parameter specifies 0 or 1 depending on the stack.
-   -----------------------------------------------------------------------*/
-
   StackElement top(int stackNum) const;
-  /*------------------------------------------------------------------------
-    Retrieve value at top of the stack specified by stackNum.
-   -----------------------------------------------------------------------*/
-
   void pop(int stackNum);
-  /*------------------------------------------------------------------------
-    Remove value at top of the specified stack.
-   -----------------------------------------------------------------------*/ 
 
  private:
   StackElement myArray[STACK_CAPACITY];
@@ -68,18 +32,27 @@ DStack::DStack()
 {}
 
 
-bool DStack::empty(int stackNum) const{
+bool DStack::isEmpty(int stackNum) const{
 }
 
-bool DStack::full() const{
+bool DStack::isFull() const{
 }
+
+// Given a value and the stack number (0 or 1) pushes the value
+// onto the stack (if it is not full)
 
 void DStack::push(int stackNum, const StackElement & value) {
 }
 
+// Given the stack number (0 or 1), returns the value of the top element
+// in that stack (if that stack is not empty)
 
 StackElement DStack::top(int stackNum) const {
+  return(0);
 }
+
+// Given the stack number (0 or 1), pops the top element
+// from that stack (if that stack is not empty)
 
 void DStack::pop(int stackNum) {
 }
@@ -113,7 +86,7 @@ int main(int argc, char *argv[]) {
 		switch(inst) {
 			case 'i': 
 				cin >> a >> b;
-				if (s.full()) 
+				if (s.isFull()) 
 					cout << "Can't push " << b << " to stack #" << a << ", it is full!\n"; 
 				else  { 
 					cout << "Inserting " << b << " to stack #" << a << endl;
@@ -122,7 +95,7 @@ int main(int argc, char *argv[]) {
 				break;
 			case 'r': 
 				cin >> a;
-				if (s.empty(a)) 
+				if (s.isEmpty(a)) 
 					cout << "Can't pop, stack #" << a << " is empty!\n";
 				else { 
 					cout << "Poping from stack #" << a << endl;
@@ -136,7 +109,7 @@ int main(int argc, char *argv[]) {
 			case 'a':
 				cin >> a;
 				cout << "Emptying all of stack #" << a << ": ";
-				while(!s.empty(a)) {
+				while(!s.isEmpty(a)) {
 					cout << s.top(a) << " "; 
 					s.pop(a);
 				}
